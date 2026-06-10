0805fad12b8989b261fdc800ec042f21051a28a7
thanhtungtav4 <build@local>
2026-06-10 20:06:31 +0700

    fix: add material-icons-extended dependency for compile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.work:work-runtime-ktx:2.10.0")
